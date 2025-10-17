a1901785575f507a5d1d6ac132cccf0fbdee7d70
Sigmond Kukla <sigkukla@gmail.com>
2025-10-17 10:17:12 -0400

    Add external LED demo

---
+++ examples/external_led/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+const int led_pin = 9;
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led_pin, HIGH);
+  delay(1000);
+  digitalWrite(led_pin, LOW);
+  delay(1000);
+}
